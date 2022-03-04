5faea3162d338c8af6aa3a60f82e216c571bd2e6
Cleomar Joaquim <cleojoaquim@hotmail.com>
2022-03-04 11:10:36 -0300

    injeção de dependencia database seeding

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,7 +1,6 @@
 package com.educandoweb.course.resources;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.StreamingHttpOutputMessage.Body;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.educandoweb.course.entities.User;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value= "/users")
 public class UserResource {
 	@GetMapping
 	public ResponseEntity<User> findAll(){
-		User  user = new User(1L, "maria", "maria@gamil", "99999999", "senha");
-		return ResponseEntity.ok().body(user);
+		User  u = new User(1L, "maria", "maria@gamil", "99999999", "senha");
+		return ResponseEntity.ok().body(u);
 	} 
 }
